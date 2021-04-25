d1d6cfb5fe6947f17050b1db3c5f760548c7828a
yacchiDAO <okamura@net.lab.uec.ac.jp>
2021-04-25 21:03:07 +0900

    fix auth

--- rumbl/lib/rumbl_web/router.ex
+++ rumbl/lib/rumbl_web/router.ex
@@ -7,7 +7,7 @@ defmodule RumblWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug RumblWeb.Auth, repo: RumblWeb.Repo
+    plug RumblWeb.Auth, repo: Rumbl.Repo
   end
 
   pipeline :api do
